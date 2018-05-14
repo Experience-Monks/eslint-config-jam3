366c3bb62cbef241753657c6c9baac0e71dff8c8
Iran Reyes <iran.reyes@jam3.com>
2018-05-13 21:34:20 -0400

    Extend basic rules

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+'use strict';
+
 module.exports = {
-  plugins: ['prettier'],
-  extends: ['react-app', 'prettier'],
+  plugins: ['prettier', 'promise', 'import', 'jsx-a11y'],
+  extends: ['eslint:recommended', 'prettier'],
   rules: {
-    'prettier/prettier': 'error'
+    'prettier/prettier': 'error',
+    'jsx-a11y/href-no-hash': 'off',
+    'promise/param-names': 'error',
+    'promise/no-return-wrap': 'error',
+    'import/no-webpack-loader-syntax': 'off',
+    'import/no-unresolved': ['error', { ignore: ['svg-inline-loader'] }],
+    'import/named': 'error',
+    'import/default': 'error',
+    'import/first': 'error',
+    'import/order': ['error', { groups: ['builtin', 'external', ['parent', 'sibling', 'index']] }],
+    'import/no-extraneous-dependencies': [
+      'warn',
+      { devDependencies: true, optionalDependencies: true, peerDependencies: false }
+    ]
   }
 };
