206192aa1131d9e3af1da6c35b9187d4a8013796
Iran Reyes <iran.reyes@jam3.com>
2018-05-14 23:39:34 -0400

    Add rules documentation

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 'use strict';
 
 module.exports = {
+  parser: 'babel-eslint',
+
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true
+  },
+
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      generators: true,
+      experimentalObjectRestSpread: true
+    }
+  },
+
   plugins: ['prettier', 'promise', 'import', 'jsx-a11y'],
+
   extends: ['eslint:recommended', 'prettier'],
+
   rules: {
+    // https://github.com/prettier/eslint-plugin-prettier
     'prettier/prettier': 'error',
+
+    // https://github.com/evcohen/eslint-plugin-jsx-a11y/tree/master/docs/rules
     'jsx-a11y/href-no-hash': 'off',
+
+    // https://github.com/xjamundx/eslint-plugin-promise#rules
     'promise/param-names': 'error',
     'promise/no-return-wrap': 'error',
+
+    // https://github.com/benmosher/eslint-plugin-import/tree/master/docs/rules
     'import/no-webpack-loader-syntax': 'off',
     'import/no-unresolved': ['error', { ignore: ['svg-inline-loader'] }],
     'import/named': 'error',
